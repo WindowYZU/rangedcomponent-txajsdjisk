6d34aef27094e8bfa9f99c014177a8cba7c3afb1
user <user@007>
2018-10-09 14:58:14 +0800

    PP

--- src/main/java/lendle/courses/wp/rangedcomponentsamples/Slider1.java
+++ src/main/java/lendle/courses/wp/rangedcomponentsamples/Slider1.java
@@ -36,6 +36,15 @@ public class Slider1 {
         //majorTickSpacing to 
         //
         //and then add the slider to frame
+        slider.setMinimum(0);
+        slider.setMaximum(100);
+        slider.setValue(50);
+        slider.setPaintTicks(true);
+        slider.setPaintTrack(true);
+        slider.setPaintLabels(true);
+        slider.setMinorTickSpacing(10);
+        slider.setMajorTickSpacing(20);
+        frame.add(slider);
         
         //////////////////////////////////
         
